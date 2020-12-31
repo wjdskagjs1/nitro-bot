613dc5e22510475f9b6477994f1b82d763892bc7
wjdskagjs1 <wjdskagjs1@naver.com>
2020-12-31 17:10:22 +0900

    commit

---
+++ index.js
@@ -0,0 +1,116 @@
+const Discord = require('discord.js');
+const discordClient = new Discord.Client();
+const config = require('./config.json');
+const fs = require('fs');
+const article = fs.readFileSync("README.md").toString();
+
+const mongoose = require('mongoose')
+
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(config.mongouri,connectionParams)
+.then( () => {
+    console.log('Connected to database ');
+})
+.catch( (err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+});
+
+discordClient.on('ready', () => {
+  console.log(`Logged in as ${discordClient.user.tag}!`);
+});
+
+const CommandSchema = new mongoose.Schema({
+    command: String,
+    link: String,
+});
+const Command = mongoose.model("command", CommandSchema);
+
+discordClient.on('message', msg => {
+    const prefix = '>';
+    const commands = msg.content.split(' ');
+    const filterCommands = ['help'];
+    if(commands[0][0] !== prefix){
+        return;
+    }
+
+    if(commands.length === 1){
+        if(commands[0] === prefix+'help'){
+            msg.channel.send("\`\`\`"+article+"\`\`\`");
+            return;
+        }
+        if(commands[0][0] === prefix){
+            const command = commands[0].substr(1);
+            let filtered = false;
+            filterCommands.forEach((element)=>{
+                if(element === command){
+                    filtered = true;
+                }
+            });
+            if(filtered){
+                msg.channel.send('예약어는 사용할 수 없습니다.');
+                return;
+            }
+            Command.findOne({command: command}, (err, data)=>{
+                if(err){
+                    console.log(err);
+                }else{
+                    if(data === null){
+                        console.log('nothing');
+                    }else{
+                        msg.channel.send(data.link);
+                        //msg.channel.send({ files: [ data.link ] });
+                    }
+                }
+            });
+            return;
+        }
+    }
+    if(commands.length === 2){
+        const filtered = false;
+        const command = commands[0].substr(1);
+        const link = commands[1];
+
+        if(!command || !link){
+            msg.channel.send('인수가 부족합니다.');
+            return;
+        }
+        
+        filterCommands.forEach((element)=>{
+            if(prefix+element === command){
+                filtered = true;
+            }
+        });
+        if(filtered){
+            msg.channel.send('이미 있는 명령어입니다.');
+            return;
+        }
+
+        Command.findOne({command: command}, (err, data)=>{
+            if(err){
+                console.log(err);
+            }else{
+                if(data === null){
+                    const newCommand = new Command({
+                        command: command,
+                        link: link,
+                    });
+                    newCommand.save();
+                }else{
+                    Command.update({command: command}, {$set: { link: link }});
+                }
+            }
+        });
+        return;
+    }
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
+
+discordClient.on("error", () => { console.log("error"); });
+
+discordClient.login(config.token);
